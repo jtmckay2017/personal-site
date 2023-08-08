8e94ff51512dc9406c85f38231a8093dd9e5614a
Joel McKay <jtmckay2017@gmail.com>
2023-08-08 15:34:18 -0400

    feat: project section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-site';
+
+
+  projects = [
+    {
+      title: 'Farmers Market', 
+      description:`A simple game about growing and selling farm plants.\n\n
+
+      Made in Unity3d using C# for the 2022 Winter My First Game Jam.`, 
+      imgUrl: 'https://img.itch.zone/aW1nLzgwMDg4NjIucG5n/315x250%23c/ndB%2BQJ.png'
+    },
+    {
+      title: 'Farmers Market', 
+      description:`A simple game about growing and selling farm plants.\n\n
+
+      Made in Unity3d using C# for the 2022 Winter My First Game Jam.`, 
+      imgUrl: 'https://img.itch.zone/aW1nLzgwMDg4NjIucG5n/315x250%23c/ndB%2BQJ.png'
+    },
+
+  ]
 }
